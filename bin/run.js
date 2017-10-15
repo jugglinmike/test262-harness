c3669648844bdb4f2b1e5917fe9010f824b9339e
Mike Pennisi <mike@mikepennisi.com>
2017-10-14 22:38:17 -0400

    PARTIAL: demo integration with `test262-stream`

--- bin/run.js
+++ bin/run.js
@@ -4,7 +4,6 @@
 // This code is governed by the BSD License found in the LICENSE file.
 const DEFAULT_TEST_TIMEOUT = 10000;
 
-const compile = require('test262-compiler');
 const fs = require('fs');
 const Path = require('path');
 const globber = require('../lib/globber.js');
@@ -16,7 +15,6 @@ const util = require('util');
 const resultsEmitter = require('../lib/resultsEmitter.js');
 const agentPool = require('../lib/agentPool.js');
 const test262Finder = require('../lib/findTest262.js');
-const scenariosForTest = require('../lib/scenarios.js');
 
 // test262 directory (used to locate includes unless overridden with includesDir)
 let test262Dir = argv.test262Dir;
@@ -111,43 +109,51 @@ if (!argv._.length) {
   return;
 }
 
-// Test Pipeline
-const pool = agentPool(Number(argv.threads), hostType, argv.hostArgs, hostPath,
-                       { timeout: argv.timeout, transpiler });
-const paths = globber(argv._);
-if (!includesDir && !test262Dir) {
-  test262Dir = test262Finder(paths.fileEvents[0]);
+globber(argv._)
+  .reduce((a, x) => a.concat(x), [])
+  .toPromise()
+  .then(run)
+  .catch(error => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+
+function run(paths) {
+  if (!includesDir && !test262Dir) {
+    test262Dir = test262Finder(paths);
+  }
+
+  // Test Pipeline
+  const pool = agentPool(Number(argv.threads), hostType, argv.hostArgs,
+                         hostPath, { timeout: argv.timeout, transpiler });
+  const scenarios = new Rx.Subject();
+  new (require('../../test262-stream'))(test262Dir, { paths, includesDir })
+    .on('data', (test) => scenarios.onNext(test))
+    .on('error', (error) => scenarios.onError(error))
+    .on('end', () => scenarios.onCompleted());
+  const pairs = Rx.Observable.zip(pool, scenarios.map(insertPrelude));
+  const rawResults = pairs.flatMap(pool.runTest).tapOnCompleted(() => pool.destroy());
+  const results = rawResults.map(test => {
+    test.result = validator(test);
+    return test;
+  });
+  const resultEmitter = resultsEmitter(results);
+  reporter(resultEmitter, reporterOpts);
 }
-const files = paths.map(pathToTestFile);
-const tests = files.map(compileFile);
-const scenarios = tests.flatMap(scenariosForTest);
-const pairs = Rx.Observable.zip(pool, scenarios);
-const rawResults = pairs.flatMap(pool.runTest).tapOnCompleted(() => pool.destroy());
-const results = rawResults.map(test => {
-  test.result = validator(test);
-  return test;
-});
-const resultEmitter = resultsEmitter(results);
-reporter(resultEmitter, reporterOpts);
 
 function printVersion() {
   const p = require(Path.resolve(__dirname, "..", "package.json"));
   console.log(`v${p.version}`);
 }
 
-function pathToTestFile(path) {
-  return { file: path, contents: fs.readFileSync(path, 'utf-8')};
-}
+function insertPrelude(test) {
+  const index = test.insertionIndex;
 
-function compileFile(test) {
-  const endFrontmatterRe = /---\*\/\r?\n/g;
-  const match = endFrontmatterRe.exec(test.contents);
-  if (match) {
-    test.contents = test.contents.slice(0, endFrontmatterRe.lastIndex)
+  if (index > -1) {
+    test.contents = test.contents.slice(0, index)
                     + preludeContents
-                    + test.contents.slice(endFrontmatterRe.lastIndex);
-  } else {
-    test.contents = preludeContents + test.contents;
+                    + test.contents.slice(index);
   }
-  return compile(test, { test262Dir, includesDir });
+
+  return test;
 }
